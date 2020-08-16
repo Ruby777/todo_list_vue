c1285074a053ce7dbb31fe1659120746f5ae8ccf
Lucy <lucynn11@gmail.com>
2020-08-16 22:20:00 +0400

    adding vue binding

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -1,12 +1,15 @@
 <template>
-    <div>
-        <p>Todo Item</p>
+    <div class="todo-item" v-bind:class="{'is-complete':todo.completed}">
+        <p>
+            <input type="checkbox" v-on:change="markComplete">
+            {{todo.hljs-title}}</p>
     </div>
 </template>
 
 <script>
 export default {
-    name: "TodoItem"
+    name: "TodoItem",
+    props: ["todo"]
 }
 </script>
 
