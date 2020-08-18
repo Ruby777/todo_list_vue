254d3e97b049aab3d001a81dda2fb532c83fa458
Lucy <lucynn11@gmail.com>
2020-08-18 14:31:50 +0400

    delete feature

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -2,14 +2,21 @@
     <div class="todo-item" v-bind:class="{'is-complete':todo.completed}">
         <p>
             <input type="checkbox" v-on:change="markComplete">
-            {{todo.hljs-title}}</p>
+            {{todo.title}}
+            <button @click="$emit('del-todo', todo.id)" class="del">x</button>
+        </p>
     </div>
 </template>
 
 <script>
 export default {
     name: "TodoItem",
-    props: ["todo"]
+    props: ["todo"],
+    method: {
+        markComplete() {
+            this.todo.completed  = !this.todo.completed;
+        }
+    }
 }
 </script>
 
